6a586ab5c13a7e84908dec32d3db1bea96c5d9aa
Bhargav Sonagra <sonagrabhargav@gmail.com>
2023-03-23 12:36:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+
+let Weight = document.querySelector("#weight");
+let height = document.querySelector("#height");
+let submit = document.querySelector("#submit");
+let bmiValue = document.querySelector("#bmiValue");
+let bmiText = document.getElementById("bmiText");
+let bmi = document.getElementById("bmi");
+let img = document.getElementById("img");
+
+
+submit.addEventListener("click", (e) => {
+    bmi.style.color = "black";
+    let WeightVal = Weight.value;
+    let heightVal = height.value;
+    let result = (((WeightVal) / (heightVal * heightVal)) * 10000).toFixed(1);
+
+    bmiValue.innerHTML = result;
+
+    if (result < 18.5) {
+        console.log("Underweight")
+        bmiValue.style.borderColor = "#eba834"
+        bmiText.style.color = "#eba834"
+        bmiText.innerHTML = "Underweight"
+
+    } else if (result >= 18.5 && result < 24.9) {
+        bmiValue.style.borderColor = "green"
+        bmiText.style.color = "green"
+        console.log("Normal")
+        bmiText.innerHTML = "Normal"
+
+    } else if (result >= 25 && result < 29.9) {
+        bmiValue.style.borderColor = "#eb6134"
+        bmiText.style.color = "#eb6134"
+        console.log("Overweight")
+        bmiText.innerHTML = "Overwight"
+
+    } else if (result > 30) {
+        bmiValue.style.borderColor = "#ad242d"
+        bmiText.style.color = "#ad242d"
+        console.log("Obese")
+        bmiText.innerHTML = "Obese"
+    }
+
+})
+
+// Close Button pop up
+
+let closebtn = document.getElementById("closebtn");
+closebtn.addEventListener('click', () => {
+    high_converotr.style.display = "none";
+})
+
+// Click Here Button
+
+let clickhere = document.getElementById("clickhere");
+clickhere.addEventListener('click', () => {
+    high_converotr.style.display = "block";
+})
+
+// foot and cm convert in pop up
+
+let foot = document.getElementById("foot");
+let cm = document.getElementById("cm");
+
+foot.addEventListener('input', function () {
+    let foot1 = parseFloat(this.value);
+    cm.value = ((foot1) * 30.48).toFixed(2);
+})
+// Copy Button in pop up
+let copybtn = document.getElementById("copybtn");
+copybtn.addEventListener('click', () => {
+    height.value = cm.value;
+    high_converotr.style.display = "none";
+})
